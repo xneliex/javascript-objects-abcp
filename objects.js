cbca703618a99720cb083ffe56fbbc9f5cc51472
Janeliz Sanders <clonegirl20@gmail.com>
2018-01-17 01:04:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,17 +1,2 @@
-var playlist = {"rock":"my chemical romance"};
-var playlist=new object{"rock:my chemical romance"}
-var playlist={
-  "my chemical romance"
-  "the used"
-}
-const cool="rock"
-var disco:"panic at the disco",
-string["my string"]=2
-};
-function updatePlaylist (playlist, rock, disco){
-  object.assign({},
-  return playlist
-};
-function removeFromPlaylist ( rock, disco){
-  return playlist
-}
+var playlist = {'rock':'my chemical romance'};
+
