c9ca89bf8626fb45db5b69b319b83698afceb6af
Janeliz Sanders <clonegirl20@gmail.com>
2018-01-17 01:09:57 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'rock':'my chemical romance'};
+var artistname="my chemial romance"
+function updatePlaylist(object,key, value){
+  return object.assign({}, object, {[key]:value{
+
+  }
+function removeFromPlaylist(playlist, artistname){
+  delete playlist [artistname]
+  return playlist
+}
