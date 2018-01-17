61d2eafc815d7658d29970ef14650ae4d959cd13
Janeliz Sanders <clonegirl20@gmail.com>
2018-01-17 00:44:02 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {"rock""disco"
+};
+rock:"hoobstank",
+disco:"panic at the disco",
+
+};
+function updatePlaylist (playlist, artist, title){
+  objectassing({})
+  return playlist
+};
+function removeFromPlaylist (playlist, artist, title){
+  return playlist
+}
