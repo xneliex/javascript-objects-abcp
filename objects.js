a431bc19bd70e8e9866ee0157ee73e75033e26ea
Janeliz Sanders <clonegirl20@gmail.com>
2018-01-17 01:08:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,9 @@
 var playlist = {'rock':'my chemical romance'};
 function updatePlaylist(object,key, value){
   return object.assign({}, object, {[key]:value{
-    
+
   }
-function removeFromPlaylist
+function removeFromPlaylist(playlist, artistname){
+  delete playlist [artistname]
+  return playlist
+}
