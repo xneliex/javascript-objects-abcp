8a04c85be9a298ab2083797115554f0c8b0b322e
Janeliz Sanders <clonegirl20@gmail.com>
2018-01-17 00:49:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {"rock""disco"
 };
 var rock:"hoobstank",
 var disco:"panic at the disco",
-
+string["my string"]=2
 };
 function updatePlaylist (playlist, rock, disco){
   object.assign({},
